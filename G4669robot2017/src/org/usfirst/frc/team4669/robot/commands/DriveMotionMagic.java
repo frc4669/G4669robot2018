dbaaf8d13ce200ffbb0034b51a1b38937e0670c3
tand <tand@tand-mba-pc>
2017-02-25 14:54:32 -0800

    add regular Drive Motion Magic command

--- G4669robot2017/src/org/usfirst/frc/team4669/robot/commands/DriveMotionMagic.java
+++ G4669robot2017/src/org/usfirst/frc/team4669/robot/commands/DriveMotionMagic.java
@@ -3,14 +3,14 @@ package org.usfirst.frc.team4669.robot.commands;
 import org.usfirst.frc.team4669.robot.Robot;
 import org.usfirst.frc.team4669.robot.RobotMap;
 
-import edu.wpi.first.wpilibj.command.InstantCommand;
+import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
-public class DriveMotionMagic extends InstantCommand {
+public class DriveMotionMagic extends Command {
 
-    private double distanceToTravel;
+	private double distanceToTravel;
 
 	public DriveMotionMagic(double distance) {
         super();
@@ -25,4 +25,21 @@ public class DriveMotionMagic extends InstantCommand {
     	Robot.driveTrain.driveMotionMagic(distanceToTravel);
     }
 
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
 }
